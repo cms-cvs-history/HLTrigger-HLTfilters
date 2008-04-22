2c97f02a5c9340473bcd1db193bdb210f13a2432
cmsbuild <>
2008-04-22 08:00:55 +0000

    This commit was manufactured by cvs2git to create tag 'HLTrigger- HLTfilters-V01-00-15-01'.
    Sprout from master 2008-04-22 08:00:54 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'Update of filters for new triggersummaryAOD config.'
    Cherrypick from master 2008-03-28 09:48:29 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'adding new py configs':
        python/__init__.py
        python/hlt1CaloMET_cfi.py
        python/hltHighLevel_cfi.py

--- python/hltHighLevel_cfi.py
+++ python/hltHighLevel_cfi.py
@@ -1,8 +1,10 @@
+# The following comments couldn't be translated into the new config version:
+
+# provide list of HLT paths you want
 import FWCore.ParameterSet.Config as cms
 
 hltHighLevel = cms.EDFilter("HLTHighLevel",
-    HLTPaths = cms.vstring(), ## provide list of HLT paths you want
-
+    HLTPaths = cms.vstring(),
     andOr = cms.bool(True), ## true: OR of those on your list 
 
     TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
